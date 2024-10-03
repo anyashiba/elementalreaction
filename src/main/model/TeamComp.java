b2680a8a9b07bbf2d50e15b80821ae25585504b8
Anna Si Tu <asitu03@student.ubc.ca>
2024-10-03 11:58:25 -0700

    Added specifications for TeamComp class

---
+++ src/main/model/TeamComp.java
@@ -0,0 +1,22 @@
+package model;
+
+import java.util.ArrayList;
+
+public class TeamComp {
+    private ArrayList<Element> team;
+
+    //REQUIRES: 
+    //MODIFIES: this
+    //EFFECTS: constructs an empty team comp and ensures size of list must be < 4
+    public TeamComp() {
+        //stub
+    }
+
+    //REQUIRES: 
+    //MODIFIES: this
+    //EFFECTS: adds character to the team
+    public void addCharacter() {
+        //stub
+    }
+
+}
