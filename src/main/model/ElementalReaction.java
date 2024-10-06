d9948b48eaed985af554bbd052482d5e8f8ca126
Anna Si Tu <asitu03@student.ubc.ca>
2024-10-05 17:15:20 -0700

    Added tests for ElementalReaction

--- src/main/model/ElementalReaction.java
+++ src/main/model/ElementalReaction.java
@@ -7,12 +7,14 @@ public class ElementalReaction {
     private static final int FROZEN = 12;
     private static final int SUPERCONDUCT = 9;
     private static final int VAPORIZE = 18;
-    private static final int OVERLOADED = 10;
+    private static final int OVERLOADED = 8;
     private static final int BURNING = 5;
     private static final int BLOOM = 15;
-    private static final int ELECTROCHARGED = 11;
+    private static final int ELECTROCHARGED = 8;
     private static final int QUICKEN = 15;
     private static final int CRYSTALLIZE = 7;
+    private static final int SWIRL = 10;
+    private static final int BASE = 2;
 
     private ArrayList<String> battleLog;
 
@@ -25,12 +27,15 @@ public class ElementalReaction {
 
     //REQUIRES: 
     //MODIFIES: this
-    //EFFECTS: call abilities using method, adds the abilities to the battlelog,
-    // and it will return the value of damage it did
+    //EFFECTS: call abilities using method, adds the abilities to the battlelog
+    //how much dmg it did, and it will return the value of damage it did
     public int react(Element ability1, Element ability2) {
         return 1; //stub
     }
 
+    //REQUIRES: 
+    //MODIFIES: this
+    //EFFECTS: returns the battlelog of abilties casted and the damage it dealt
     public ArrayList<String> getLog() {
         return null; //stub
     }
--- src/test/model/TestGenshin.java
+++ src/test/model/TestGenshin.java
@@ -7,6 +7,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.zip.CRC32;
+
+import model.ElementalReaction;
 
 public class TestGenshin {
     private Character testCharacter;
@@ -59,4 +62,107 @@ public class TestGenshin {
         assertEquals(0, testER.getLog().size());
     }
 
+    @Test
+    void testERgetLog() {
+        ArrayList<String> testLog = new ArrayList<>();
+        testLog.add("ability1");
+        assertEquals("ability1", testLog.get(0));
+        assertEquals(1, testER.getLog().size());
+    }
+
+    void testERgetLogMultiple() {
+        ArrayList<String> testLog = new ArrayList<>();
+        testLog.add("ability1");
+        testLog.add("ability2");
+        assertEquals("ability1", testLog.get(0));
+        assertEquals("ability1", testLog.get(1));
+        assertEquals(2, testER.getLog().size());
+    }
+
+    @Test 
+    void testERreact() {
+        Element e1 = new Element("Dendro", "ability1");
+        Element e2 = new Element("Hydro", "ability2");
+        assertEquals(15, testER.react(e1, e2));
+        assertEquals("ability1 and ability 2 did 15 damage!", testER.getLog().get(0));
+        assertEquals(1, testER.getLog().size());
+    }
+
+    @Test 
+    void testERreactMultiple() {
+        Element e1 = new Element("Dendro", "ability1");
+        Element e2 = new Element("Hydro", "ability2");
+        Element e3 = new Element("Pyro", "ability3");
+        assertEquals(15, testER.react(e1, e2));
+        assertEquals(5, testER.react(e1, e3));
+        assertEquals("ability1 and ability 2 did 15 damage!", testER.getLog().get(0));
+        assertEquals("ability1 and ability 3 did 5 damage!", testER.getLog().get(1));
+        assertEquals(2, testER.getLog().size());
+    }
+
+    void testERreactAll() {
+        Element Dendro = new Element("Dendro", "ability1");
+        Element Hydro = new Element("Hydro", "ability2");
+        Element Pyro = new Element("Pyro", "ability3");
+        Element Anemo = new Element("Anemo", "ability4");
+        Element Electro = new Element("Electro", "ability5");
+        Element Geo = new Element("Geo", "ability6");
+        Element Cryo = new Element("Cryo", "ability7");
+        //Dendro
+        assertEquals(2, testER.react(Dendro, Dendro));
+        assertEquals(15, testER.react(Dendro, Hydro));
+        assertEquals(5, testER.react(Dendro, Pyro));
+        assertEquals(2, testER.react(Dendro, Anemo));
+        assertEquals(15, testER.react(Dendro, Electro));
+        assertEquals(2, testER.react(Dendro, Geo));
+        assertEquals(2, testER.react(Dendro, Cryo));
+        //Hydro
+        assertEquals(15, testER.react(Hydro, Dendro));
+        assertEquals(2, testER.react(Hydro, Hydro));
+        assertEquals(18, testER.react(Hydro, Pyro));
+        assertEquals(8, testER.react(Hydro, Anemo));
+        assertEquals(8, testER.react(Hydro, Electro));
+        assertEquals(7, testER.react(Hydro, Geo));
+        assertEquals(12, testER.react(Hydro, Cryo));
+        //Pyro
+        assertEquals(5, testER.react(Pyro, Dendro));
+        assertEquals(18, testER.react(Pyro, Hydro));
+        assertEquals(2, testER.react(Pyro, Pyro));
+        assertEquals(10, testER.react(Pyro, Anemo));
+        assertEquals(8, testER.react(Pyro, Electro));
+        assertEquals(7, testER.react(Pyro, Geo));
+        assertEquals(20, testER.react(Pyro, Cryo));
+        //Anemo
+        assertEquals(2, testER.react(Anemo, Dendro));
+        assertEquals(10, testER.react(Anemo, Hydro));
+        assertEquals(10, testER.react(Anemo, Pyro));
+        assertEquals(2, testER.react(Anemo, Anemo));
+        assertEquals(10, testER.react(Anemo, Electro));
+        assertEquals(2, testER.react(Anemo, Geo));
+        assertEquals(10, testER.react(Anemo, Cryo));
+        //Electro
+        assertEquals(15, testER.react(Electro, Dendro));
+        assertEquals(8, testER.react(Electro, Hydro));
+        assertEquals(8, testER.react(Electro, Pyro));
+        assertEquals(10, testER.react(Electro, Anemo));
+        assertEquals(2, testER.react(Electro, Electro));
+        assertEquals(7, testER.react(Electro, Geo));
+        assertEquals(9, testER.react(Electro, Cryo));
+        //Geo
+        assertEquals(2, testER.react(Geo, Dendro));
+        assertEquals(7, testER.react(Geo, Hydro));
+        assertEquals(7, testER.react(Geo, Pyro));
+        assertEquals(2, testER.react(Geo, Anemo));
+        assertEquals(7, testER.react(Geo, Electro));
+        assertEquals(2, testER.react(Geo, Geo));
+        assertEquals(7, testER.react(Geo, Cryo));
+        //Cryo
+        assertEquals(2, testER.react(Cryo, Dendro));
+        assertEquals(12, testER.react(Cryo, Hydro));
+        assertEquals(20, testER.react(Cryo, Pyro));
+        assertEquals(10, testER.react(Cryo, Anemo));
+        assertEquals(9, testER.react(Cryo, Electro));
+        assertEquals(7, testER.react(Cryo, Geo));
+        assertEquals(2, testER.react(Cryo, Cryo));
+    }
 }
